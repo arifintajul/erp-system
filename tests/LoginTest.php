39a4b7b5671f811f90e0aabb592e4e6460a4927d
arifintajul <107012387+arifintajul@users.noreply.github.com>
2024-09-21 17:11:51 +0700

    Deskripsi singkat tentang perubahan 4

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,19 +1,3 @@
 ECHO is off.
-<?php
-/* use PHPUnit\Framework\TestCase;
 
-class LoginTest extends TestCase {
-    public function testValidLogin() {
-        $loginController = new LoginController();
-        $result = $loginController->login('validUser', 'validPassword');
-        $this->assertTrue($result);
-    }
-
-    public function testInvalidLogin() {
-        $loginController = new LoginController();
-        $result = $loginController->login('invalidUser', 'wrongPassword');
-        $this->assertFalse($result);
-    }
-}
- */?>
 
